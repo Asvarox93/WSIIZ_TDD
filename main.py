8ba9ad86bb1145e9272cad2d4864f058696782ca
Sebastian Białek <asvarox93@gmail.com>
2022-03-30 18:11:42 +0200

    Implementacja wyjątku dla list mniejszych niż 2

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ def palindrome(arr):
     if type(arr) is not list:
         raise ValueError("Wrong input type! It must be a list")
 
+    if len(arr) < 2:
+        raise ValueError("List with elements is too short!")
