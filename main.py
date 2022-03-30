bc8221798f79866dfbdf77c9302bd073f637c662
Sebastian Białek <asvarox93@gmail.com>
2022-03-30 19:17:45 +0200

    Implementacja sprawdzenia czy element jest palindromem

--- main.py
+++ main.py
@@ -5,7 +5,24 @@ def palindrome(arr):
     if len(arr) < 2:
         raise ValueError("List with elements is too short!")
 
+    counter = 0
     for a in arr:
         if type(a) is not str or a.isnumeric():
             raise ValueError("Elements in list are not type of string or is numeric")
+
+        el = a.lower().replace(" ", "").replace(",", "").replace(".", "")
+
+        reversed_arr = []
+        index = len(el)
+        while index > 0:
+            reversed_arr += el[index - 1]
+            index = index - 1
+
+        reversed_el = ""
+        reversed_el = reversed_el.join(reversed_arr)
+
+        if el == reversed_el:
+            counter += 1
+
+    return counter
 
